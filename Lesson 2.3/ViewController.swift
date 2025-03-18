906aa91ae7b37d9a0228253459693e2dd5f8db01
SloA <m1_artemi@mail.ru>
2025-03-18 18:43:23 -0400

    UI and logic

--- Lesson 2.3/ViewController.swift
+++ Lesson 2.3/ViewController.swift
@@ -9,11 +9,54 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var colorView: UIView!
+    
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    private var colorControlsMap: [UISlider: UILabel] = [:]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        colorView.layer.cornerRadius = 10
+        
+        colorControlsMap = [
+            redSlider: redLabel,
+            greenSlider: greenLabel,
+            blueSlider: blueLabel
+        ]
+        
+        for slider in colorControlsMap.keys {
+            updateTitle(for: slider)
+        }
+        
+        updateColorView()
     }
 
+    @IBAction func colorSliderChanged(_ sender: UISlider) {
+        updateTitle(for: sender)
+        updateColorView()
+    }
+    
+    private func updateTitle(for slider: UISlider) {
+        colorControlsMap[slider]?.text = String(format: "%.2f", slider.value)
+    }
+    
+    private func updateColorView() {
+        
+        colorView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1.0
+        )
+    }
 
 }
 
